1c947733c469a4fc2ae418b23c14e7aaf023d9fd
Vaitul Bhayani <vaitulb@gmail.com>
2023-04-09 14:23:32 +0530

    added reviews

--- js/main.js
+++ js/main.js
@@ -116,15 +116,15 @@
 	});
 
 
-	var slides = document.querySelectorAll('#slides .slide');
-	var currentSlide = 0;
-	var slideInterval = setInterval(nextSlide, 2000);
+	// var slides = document.querySelectorAll('#slides .slide');
+	// var currentSlide = 0;
+	// var slideInterval = setInterval(nextSlide, 3500);
 
-	function nextSlide() {
-		slides[currentSlide].className = 'slide';
-		currentSlide = (currentSlide + 1) % slides.length;
-		slides[currentSlide].className = 'slide showing';
-	}
+	// function nextSlide() {
+	// 	slides[currentSlide].className = 'slide';
+	// 	currentSlide = (currentSlide + 1) % slides.length;
+	// 	slides[currentSlide].className = 'slide showing';
+	// }
 
 
 })(jQuery);
